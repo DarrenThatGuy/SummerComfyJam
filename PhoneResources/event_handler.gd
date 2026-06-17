4ac41e48359480a991b69c2845dbc342d84ae67e
Jkokol2000 <jameskokol2000@gmail.com>
2026-06-16 18:37:00 -0600

    Completed Events for neutral ending

--- PhoneResources/event_handler.gd
+++ PhoneResources/event_handler.gd
@@ -5,8 +5,8 @@ var previous_scene
 var current_scene
 
 func _ready():
-	current_scene = "res://main_screen.tscn"
-	#DialogueManager.show_example_dialogue_balloon(load("res://GameDialogue.dialogue"), 'start')
+	current_scene = "res:///Scenes/main_screen.tscn"
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/gameDialogue/GameDialogue.dialogue"), 'start')
 func true_event(event):
 	EventTracker.events[event] += 1
 func _on_phone_container_check_events(event, scene):
@@ -14,9 +14,15 @@ func _on_phone_container_check_events(event, scene):
 	if scene != null:
 		previous_scene = current_scene
 		current_scene = scene
-	if current_scene == "res://main_screen.tscn":
-		if EventTracker.events['UnlockExampleLockedApp'] > 0:
-			print("UnlockLockedApp")
+	if current_scene == "res://Scenes/main_screen.tscn":
+		match EventTracker.events['OrderedFood']:
+			1:
+				DialogueManager.show_example_dialogue_balloon(load("res://dialogue/gameDialogue/GameDialogue.dialogue"), 'MinigameDirection')
+				EventTracker.events['OrderedFood'] += 1
+		match EventTracker.events['FirstGameCompleted']:
+			1:
+				DialogueManager.show_example_dialogue_balloon(load("res://dialogue/gameDialogue/GameDialogue.dialogue"), 'GachaGameDirection')
+				EventTracker.events['FirstGameCompleted'] += 1
 	elif current_scene == "res://example_app.tscn":
 		match EventTracker.events['EnteredExampleApp']:
 			1:
@@ -31,7 +37,15 @@ func _on_phone_container_check_events(event, scene):
 			1:
 				#DialogueManager.show_example_dialogue_balloon(load("res://GameDialogue.dialogue"), 'LockedExampleApp')
 				pass
-
+	elif current_scene == "res://Scenes/food_app.tscn":
+		match  EventTracker.events['OrderedFood']:
+			1:
+				DialogueManager.show_example_dialogue_balloon(load("res://dialogue/gameDialogue/GameDialogue.dialogue"), 'OrderedFood')
+		match  EventTracker.events['EnteredFoodApp']:
+			1:
+				EventTracker.events['EnteredFoodApp'] += 1
+				DialogueManager.show_example_dialogue_balloon(load("res://dialogue/gameDialogue/GameDialogue.dialogue"), 'OpenedFoodApp')
+		
 
 func _on_phone_container_trigger_event(event):
 	if event == "SpaceGameCompleted" or event == "FlappyBirdCompleted" or event == "PongCompleted" or event == "BreakoutCompleted":
